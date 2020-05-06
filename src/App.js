0fbc54a1f5965971ad6b5cc05eadf7ac8f627c37
Snookie <kieransnook35@gmail.com>
2020-05-06 14:49:48 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
-import Header from "./components/Header/";
-import Footer from "./components/Footer/";
-import Project from "./components/Project/";
-import About from "./components/About/";
-import Contact from "./components/Contact/";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header/index";
+import Footer from "./components/Footer/index";
+import Project from "./components/Project/index";
+import About from "./components/About/index";
+import Contact from "./components/Contact/index";
+import { BrowserRouter as Router, Route,  } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div>
         <Header />
-        <Switch>
+        
         <Route exact path="/" component={About} />
         <Route path="/project" component={Project} />
         <Route path="/contact" component={Contact} />
         <Route path="*" component={About} />
-        </Switch>
+        
         <Footer />
       </div>
     </Router>
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../Footer/style.css";
+
+function Footer() {
+    return <footer className="footer">
+        Copyright &copy; 2020
+        </footer>;
+}
+
+
+export default Footer;
